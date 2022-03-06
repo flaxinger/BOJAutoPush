7a6b79b0fcf44e0586142a9c30938b069037d530
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-03-06 11:29:08 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    int a, b;
+    cin >> a >> b;
+    cout << a-b;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //test();
+    solve();
+    return 0;
+}
